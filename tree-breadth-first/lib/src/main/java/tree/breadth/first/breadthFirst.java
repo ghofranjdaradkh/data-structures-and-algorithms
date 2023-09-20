96f3fe0b78455d4ad7f4a9fee5b083cccc8cdf3f
ghofranjdaradkh <ghofran_daradkh@hotmail.com>
2023-09-20 23:47:44 +0300

    cc17

---
+++ tree-breadth-first/lib/src/main/java/tree/breadth/first/breadthFirst.java
@@ -0,0 +1,43 @@
+package tree.breadth.first;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class breadthFirst {
+    static List<Integer> breadthFirstMethod(Node root) {
+        List<Integer> result = new ArrayList<>();
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            Node temp = queue.poll();
+            result.add(temp.data);
+            System.out.print(temp.data + " ");
+
+            //*add left child to the queue
+            if (temp.left != null) {
+                queue.add(temp.left);
+            }
+
+            //add right right child to the queue
+            if (temp.right != null) {
+                queue.add(temp.right);
+            }
+        }
+
+        return result;
+    }
+
+}
+class Node {
+    int data;
+Node left;
+Node right;
+
+    public Node(int data) {
+        this.data = data;
+        this.left = null;
+        this.right=null;
+    }
+}
